6495812d714b4d1bc8013f3013415725d42b4893
Laurits Volsgaard <laurits.volsgaard@gmail.com>
2022-09-08 12:53:17 +0200

    modified:   deploy-commands.js 	modified:   index.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,6 +12,12 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+
+rest.put(Routes.applicationCommands(clientId, guildId), { body: commands })
 	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
 	.catch(console.error);
+
+// for global commands
+//rest.put(Routes.applicationCommands(clientId), { body: [] })
+//	.then(() => console.log('Successfully deleted all application commands.'))
+//	.catch(console.error);
