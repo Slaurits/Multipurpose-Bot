16f98391bd1611c79fb21b2b66b8bc66f062e5d7
Laurits Volsgaard <laurits.volsgaard@gmail.com>
2022-10-20 17:24:13 +0200

    Added a new command called date.js 	new file:   commands/date.js

---
+++ commands/date.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('date')
+		.setDescription('Displays the date'),
+	async execute(interaction) {
+		const currentDate = new Date();
+		return interaction.reply(currentDate.toLocaleString('en-DK', {
+			hour12: false,
+		}));
+	},
+};
